bb791a3876477fc96af2d40444d98cd66b3e71ad
Trần Quang Đãng <91641923+quangdang46@users.noreply.github.com>
2022-05-02 00:19:28 +0700

    Update Main.java

--- Tuan9/Homework/Bai1/Main.java
+++ Tuan9/Homework/Bai1/Main.java
@@ -1,4 +1,26 @@
+
+import java.io.File;
+import java.io.FileWriter;
+import java.util.Scanner;
+
 public class Main {
+    public static String data(String path){
+        String tmp="";
+        try{
+            File file = new File(path);
+            Scanner sc = new Scanner(file);
+            while(sc.hasNextLine()){
+                tmp+=sc.nextLine();
+            }
+
+        }catch(Exception e){
+            System.out.println("Loi");
+            return tmp;
+        }
+        return tmp;
+    }
+
+    
     public static void main(String[] args) {
         /*
         - Get specific files by extensions from a given folder.
@@ -7,12 +29,95 @@ public class Main {
         - Append text to an existing file.
         - Find the longest word in a text file.
         */ 
-        /*
-        - Nhận các tệp cụ thể theo phần mở rộng từ một thư mục nhất định.
-        - Kiểm tra xem một tệp hoặc thư mục được chỉ định bởi tên đường dẫn có tồn tại hay không.
-        - Kiểm tra xem tên đường dẫn đã cho là một thư mục hay một tệp tin.
-        - Nối văn bản vào một tệp hiện có.
-        - Tìm từ dài nhất trong tệp văn bản.
-        */ 
+        String path="./test";
+        // Cau 1
+        
+        System.out.println("Enter a file name: ");
+        boolean exists=false;
+        String check = System.console().readLine();
+        try {
+            File file = new File(path);
+            if(file.isDirectory()){
+                File[] listFile= file.listFiles();
+                for(File f: listFile){
+                    if(check.equals(f.getName())){
+                        exists=true;
+                        break;
+                    }
+                }
+            }
+            if(!exists){
+                System.out.println("File not found");
+            }else{
+                System.out.println("File found");
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+
+        // Cau 2
+        //kiem tra duong dan la thu muc hay tep tin
+        String path1="./test//a.txt";
+        try {
+            File file = new File(path1);
+            if(file.isDirectory()){
+                System.out.println("Thu muc");
+            }else if(file.isFile()){
+                System.out.println("Tap tin");
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+
+        //Noi van ban
+        path1="./test//test1";
+        String data="";
+        try{
+            File file = new File(path1);
+            if(file.isDirectory()){
+                File[] listFile= file.listFiles();
+                for(File f: listFile){
+                    String path_tmp=path1+"//"+f.getName();
+                    data+=data(path_tmp);
+                }
+            }else{
+                data+=data(path1);
+            }
+        }catch (Exception e) {
+            e.printStackTrace();
+        }
+        //In van ban
+        try{
+            File file = new File(path);
+            File[] listFile= file.listFiles();
+            for(File f: listFile){
+                if(f.isFile()){
+                    FileWriter fw = new FileWriter(f);
+                    fw.write(data);
+                    fw.close();
+                }
+            }
+        }catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        //Tim chuoi lon nhat
+        path1 = "./test//a.txt";
+        try{
+            File file = new File(path1);
+            Scanner input = new Scanner(file);
+            int max=0;
+            while(input.hasNext()){
+                String tmp=input.next();
+                if(tmp.length()>max){
+                    max=tmp.length();
+                }
+            }
+            System.out.println("Chuoi lon nhat la: "+max);
+        }catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
